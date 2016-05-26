adf0bd39f79f9c81d5f81a2db9b9a47f958a593e
butschster <butschster@gmail.com>
2016-05-26 14:13:09 +0300

    Update routes.php

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -1,10 +1,10 @@
 <?php
 
-Route::group(['prefix' => backend_url_segment(), 'as' => 'backend.', 'middleware' => ['web', 'backend.auth']], function () {
+Route::group(['prefix' => backend_url_segment(), 'as' => 'backend.', 'middleware' => ['backend']], function () {
     Route::get('/', ['as' => 'dashboard', 'uses' => 'DashboardController@getIndex']);
 });
 
-Route::group(['as' => 'api.dashboard.widget.', 'middleware' => ['web', 'backend.auth']], function () {
+Route::group(['as' => 'api.dashboard.widget.', 'middleware' => ['backend']], function () {
     RouteAPI::get('dashboard.widget.list', ['as' => 'list', 'uses' => 'DashboardController@getWidgetList']);
     RouteAPI::get('dashboard.widget', ['as' => 'settings', 'uses' => 'API\DashboardController@getWidgetSettings']);
     RouteAPI::put('dashboard.widget', ['as' => 'put', 'uses' => 'API\DashboardController@putWidget']);
